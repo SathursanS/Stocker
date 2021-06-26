0a84c1c82c8e8d6daf4025ddb2784477b8fd7bb7
Preyansh <preyanshkachhiapk@gmail.com>
2021-06-25 21:14:08 -0400

    Created boilerplate

---
+++ Frontend/App.js
@@ -0,0 +1,21 @@
+import { StatusBar } from 'expo-status-bar';
+import React from 'react';
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function App() {
+  return (
+    <View style={styles.container}>
+      <Text>Stocker App for Eng Hack 2021!</Text>
+      <StatusBar style="auto" />
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  container: {
+    flex: 1,
+    backgroundColor: '#fff',
+    alignItems: 'center',
+    justifyContent: 'center',
+  },
+});
