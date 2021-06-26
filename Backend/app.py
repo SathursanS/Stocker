f59401b1f664ef7b1db0c4260f0a5e1425ed8ddf
adshayan <adshayan.b@gmail.com>
2021-06-25 20:17:34 -0400

    Market Value:

--- Backend/app.py
+++ Backend/app.py
@@ -15,11 +15,17 @@ def listofStocks ():
     data = json.load(f)
     return jsonify(data)
 
-# @app.route('/marketValue',methods =['GEt'])
-# def stockValues():
-#     ticker =request.json['TICKER']
-#     startDate = request.json['START']
-#     endDate = request.json['END']
+@app.route('/marketValue',methods =['GEt'])
+def stockValues():
+    ticker =request.json['TICKER']
+    startDate = request.json['START']
+    endDate = request.json['END']
+    stock = yf.Ticker(ticker)
+    history = stock.history(start=startDate, end=endDate)
+    historyToList = history.values.tolist()
+    data = jsonpify(historyToList)
+
+    return data
 
 
 if __name__ == "__main__":
