afcb117eedcbeda529a6087396f1bc90000dccc4
RishanRatna <rishanratna@hotmail.com>
2021-06-26 01:54:22 -0400

    Completed the crux of the news page. Need to add header, loading indicator, and possibly load next page of news

--- Frontend/components/navbar/tabIcon.js
+++ Frontend/components/navbar/tabIcon.js
@@ -1,12 +1,11 @@
-import React from "react";
-import { StyleSheet, Text, TouchableOpacity } from "react-native";
-import { MaterialCommunityIcons } from "@expo/vector-icons";
+import React from 'react';
+import { StyleSheet, Text, TouchableOpacity } from 'react-native';
+import { MaterialCommunityIcons } from '@expo/vector-icons';
 
 const TabIcon = ({ color, tab, onPress, icon }) => {
   return (
     <TouchableOpacity style={styles.container} onPress={onPress}>
       {icon && <MaterialCommunityIcons name={icon} size={28} color={color} />}
-      <Text style={{ color }}>{tab.name}</Text>
     </TouchableOpacity>
   );
 };
@@ -14,8 +13,8 @@ const TabIcon = ({ color, tab, onPress, icon }) => {
 const styles = StyleSheet.create({
   container: {
     flex: 1,
-    alignItems: "center",
-    justifyContent: "center",
+    alignItems: 'center',
+    justifyContent: 'center',
     padding: 5,
   },
 });
